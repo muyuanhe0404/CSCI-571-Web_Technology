25c8cf01e431d661e2f311f8d4389ad04de7444f
muyuan0404 <hemuyuan120@gmail.com>
2023-02-16 12:09:29 -0800

    gcp

--- Ticket_search/main.py
+++ Ticket_search/main.py
@@ -1,9 +1,10 @@
 from flask import Flask, jsonify,abort
 from flask import *
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static', static_url_path='')
 
 @app.route("/")
 def homepage():
+    #return "hello"
     return app.send_static_file("events.html")
 
 if __name__ == "__main__":
